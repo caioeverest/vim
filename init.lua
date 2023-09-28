02606655071dc6fef8623c91fc8fea8402c12649
Everest <caioeverest.b@gmail.com>
2023-09-28 02:15:19 -0300

    feat(cmp): Add nvim-cmp autocompletion plugin
    This commit adds the nvim-cmp autocompletion plugin to the project. The plugin provides autocompletion capabilities using various sources such as copilot, nvim_lsp, buffer, and luasnip. It also includes a snippet engine and associated sources, LSP completion capabilities, and user-friendly snippets. Additionally, it configures the appearance of the autocompletion window with rounded borders and sets up key mappings for navigation and confirmation.
    
    The changes are made in `init.lua` and a new file `cmp.lua` is added under `lua/plugins`.

--- init.lua
+++ init.lua
@@ -54,22 +54,6 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Autocompletion
-    'hrsh7th/nvim-cmp',
-    dependencies = {
-      -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip',
-
-      -- Adds LSP completion capabilities
-      'hrsh7th/cmp-nvim-lsp',
-
-      -- Adds a number of user-friendly snippets
-      'rafamadriz/friendly-snippets',
-    },
-  },
-
   -- Useful plugin to show you pending keybinds.
   { 'folke/which-key.nvim',  opts = {} },
   {
@@ -471,70 +455,6 @@ mason_lspconfig.setup_handlers {
       filetypes = (servers[server_name] or {}).filetypes,
     }
   end
-}
-
--- [[ Configure nvim-cmp ]]
--- See `:help cmp`
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
-
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert {
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_locally_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  },
-  sources = {
-    { name = 'copilot' },
-    { name = 'nvim_lsp' },
-    { name = "buffer" },
-    { name = 'luasnip' },
-  },
-  window = {
-    completion = {
-      autocomplete = false,
-      completeopt = 'menu,menuone,noinsert'
-    },
-    documentation = cmp.config.window.bordered {
-      border = "rounded",
-      winhighlight = "NormalFloat:CompeDocumentation,FloatBorder:CompeDocumentationBorder",
-      max_width = 50,
-      min_width = 50,
-      max_height = math.floor(vim.o.lines * 0.4),
-      min_height = 3,
-    }
-  }
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
